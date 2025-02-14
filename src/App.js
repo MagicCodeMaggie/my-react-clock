c61244cef02e3e85de040e4ca74cfa89b9dca683
MagicCodeMaggie <maggie.zxu@outlook.com>
2025-02-14 22:31:15 +1100

    clock with click button using useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import MyClock from '../src/my-clock';
 import './App.css';
+import React, { useRef } from 'react';
 
 function App() {
+  const pRef = useRef(null);
+  const divRef = useRef(null);
   //如果不加className能正常运行,但不会加载App.css中对App类写的样式
   return (
     <div className='App'>
       <MyClock />
-    </div>
+
+      <p style={{ fontSize: "40px", color: "red" }} ref={pRef}>Designed by Maggie Xu</p>
+      <div ref={divRef}>MagicCode Education</div>
+
+      {/*1+1={1 + 1}*/}
+      <button
+        className="btn btn-success btn-lg"
+        onClick={(e) => {
+          console.log(e);
+          pRef.current.innerHTML = "Thanks for click";
+          divRef.current.innerHTML = "Coding Club for kids";
+          console.log(pRef.current.innerHTML);
+        }
+        }> Click</button >
+    </div >
   );
 }
 
